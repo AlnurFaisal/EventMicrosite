b6667e5568d45c6fdd9561c04cbf9b7f6f6ea34a
Alnur_Faisal <alnurfaisal@outlook.com>
2018-06-09 19:49:01 +0800

    Added color code legend to the sidebar for easy identification of the event types

--- govtechapp/src/components/Details/Details.js
+++ govtechapp/src/components/Details/Details.js
@@ -1,12 +1,17 @@
 import React from "react";
+import "./details.css";
 
 const Details = props => (
   <div>
-    <p>{props.time}</p>
-    <p><strong>{props.title}</strong><br />
-    <small className="grey"><i>{props.speaker}</i></small></p>
-    <br />
+    <div>
+      <p>{props.time}  <i class={`fas fa-circle ${props.type}dot`}></i>
+      <br/>
+      <strong>{props.title}</strong><br />
+      <small className="grey"><i>{props.speaker}</i></small></p>
+      <br />
+    </div>
   </div>
+
 );
 
 export default Details;
